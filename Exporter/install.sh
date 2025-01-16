3af65ce7d9cf442def8518882ef1c37b08dda5bd
rohit roshan jena <rohit.r@iserveu.co.in>
2025-01-16 10:48:48 +0530

    -n namespace -r releaseName

--- Exporter/install.sh
+++ Exporter/install.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
-kubectl create configmap config --from-file application.yaml
-helm upgrade --install camunda camunda/camunda-platform -f values.yaml -n camunda
+# Usage function to display help
+usage() {
+  echo "Usage: $0 -n <namespace> -r <release-name>"
+  exit 1
+}
+
+# Default values
+NAMESPACE=""
+RELEASE_NAME=""
+
+# Parse arguments
+while getopts "n:r:" opt; do
+  case ${opt} in
+    n) NAMESPACE=$OPTARG ;;
+    r) RELEASE_NAME=$OPTARG ;;
+    *) usage ;;
+  esac
+done
+
+# Check if both arguments are provided
+if [[ -z $NAMESPACE || -z $RELEASE_NAME ]]; then
+  usage
+fi
+
+# Create ConfigMap
+kubectl create configmap config --from-file=application.yaml -n "$NAMESPACE"
+
+# Install/Upgrade Helm chart
+helm upgrade --install "$RELEASE_NAME" camunda/camunda-platform -f values.yaml -n "$NAMESPACE"
