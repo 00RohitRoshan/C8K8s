1361dfe01002c69cd827d5e182d127bbdd4ec8bb
rohit roshan jena <rohit.r@iserveu.co.in>
2025-01-16 10:54:09 +0530

    create namespace if it doesn't exists, use no namespace if not provided

--- Exporter/install.sh
+++ Exporter/install.sh
@@ -2,7 +2,7 @@
 
 # Usage function to display help
 usage() {
-  echo "Usage: $0 -n <namespace> -r <release-name>"
+  echo "Usage: $0 -r <release-name> [-n <namespace>]"
   exit 1
 }
 
@@ -19,13 +19,34 @@ while getopts "n:r:" opt; do
   esac
 done
 
-# Check if both arguments are provided
-if [[ -z $NAMESPACE || -z $RELEASE_NAME ]]; then
+# Ensure release name is provided
+if [[ -z $RELEASE_NAME ]]; then
+  echo "Error: Release name is required."
   usage
 fi
 
+# Create namespace if provided
+if [[ -n $NAMESPACE ]]; then
+  if ! kubectl get namespace "$NAMESPACE" &>/dev/null; then
+    echo "Namespace $NAMESPACE does not exist. Creating..."
+    kubectl create namespace "$NAMESPACE"
+  else
+    echo "Namespace $NAMESPACE already exists."
+  fi
+else
+  echo "No namespace provided. Proceeding without specifying a namespace."
+fi
+
 # Create ConfigMap
-kubectl create configmap config --from-file=application.yaml -n "$NAMESPACE"
+if [[ -n $NAMESPACE ]]; then
+  kubectl create configmap config --from-file=application.yaml -n "$NAMESPACE"
+else
+  kubectl create configmap config --from-file=application.yaml
+fi
 
 # Install/Upgrade Helm chart
-helm upgrade --install "$RELEASE_NAME" camunda/camunda-platform -f values.yaml -n "$NAMESPACE"
+if [[ -n $NAMESPACE ]]; then
+  helm upgrade --install "$RELEASE_NAME" camunda/camunda-platform -f values.yaml -n "$NAMESPACE"
+else
+  helm upgrade --install "$RELEASE_NAME" camunda/camunda-platform -f values.yaml
+fi
